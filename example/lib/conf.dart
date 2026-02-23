90603bffee38181c4d236cfed31ff27348ece995
Diebru96 <diego.bruno@saet.org>
2026-02-23 17:39:18 +0100

    removed example

--- example/lib/conf.dart
+++
@@ -1,5 +0,0 @@
-Map<String, String> servermap = {
-  'janus_ws': 'wss://janus1.januscaler.com/janus/ws',
-  'janus_rest': 'https://janus.conf.meetecho.com/janus',
-  'servercheap': 'ws://107.152.35.248:8188'
-};
